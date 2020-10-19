43257417b03c229a4341ecc50d35d0889dbf8a67
ShotAlex <shot.aleks@gmail.com>
2020-10-19 22:31:03 +0300

    feat: add changing background and greeting

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const time = document.getElementById('time'),
       name = document.getElementById('name'),
       focus = document.getElementById('focus');
 
+
+// TIME
 const showTime = () => {
   let today = new Date(),
       hour = today.getHours(),
@@ -14,5 +16,29 @@ const showTime = () => {
 }
 // showTime();
 const addZero = n => (+n < 10 ? '0' : '') + n;
-
 setInterval(showTime, 1000);
+
+// BACKGROUND AND GREETING
+const setBgGreet = () => {
+  let hour = (new Date()).getHours();
+
+  if (hour < 5 || hour > 23) {
+    // Night
+    document.body.style.backgroundImage = 'url(./assets/images/night/01.jpg)';
+    greeting.textContent = 'Good Night';
+  } else if (hour < 10) {
+    // Morning
+    document.body.style.backgroundImage = 'url(./assets/images/morning/01.jpg)';
+    greeting.textContent = 'Good Night';
+  } else if (hour < 18) {
+    // Day
+    document.body.style.backgroundImage = 'url(./assets/images/day/01.jpg)';
+    greeting.textContent = 'Good Night';
+  } else {
+    // Evening
+    document.body.style.backgroundImage = 'url(./assets/images/evening/01.jpg)';
+    greeting.textContent = 'Good Night';
+  }
+}
+
+setBgGreet();
