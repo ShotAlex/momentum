b4957641435c2169ff489d608c69ff139fcd40b1
ShotAlex <shot.aleks@gmail.com>
2020-10-21 22:36:37 +0300

    feat: add check time function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const time = document.getElementById('time'),
       name = document.getElementById('name'),
       focus = document.getElementById('focus'),
       timeDate = document.getElementById('date');
+      // nextBg = document.getElementById('btn')
 
 // TIME
 const showTime = () => {
@@ -12,6 +13,7 @@ const showTime = () => {
       sec = today.getSeconds();
 
   time.innerHTML = `${addZero(hour)}:${addZero(min)}:${addZero(sec)}`;
+  if (hour === 0 && min === 0) showNextBg();
 }
 
 // DATE
@@ -25,7 +27,6 @@ const showDate = (lang = 'en') => {
   timeDate.innerHTML = `${dayOfWeek}, ${dayNumber} ${month}`;
 }
 
-// showTime();
 const addZero = n => (+n < 10 ? '0' : '') + n;
 
 // GET RANDOM NUMBER
@@ -34,39 +35,50 @@ function getRandomNumber(min = 1, max = 20) {
   return rand = num < 10 ? '0'+num : num;
 }
 
-// BACKGROUND AND GREETING
-const setBgGreet = () => {
-  let hour = (new Date()).getHours();
-  // let numRandPic = Math.random()
-  // console.log('numRandPic : ', numRandPic);
-
-  if (hour < 6) {
-    // Night
-    document.body.style.backgroundImage = `url(./assets/images/night/${getRandomNumber()}.jpg)`;
-    greeting.textContent = 'Good Night';
-  } else if (hour < 12) {
-    // Morning
-    document.body.style.backgroundImage = `url(./assets/images/morning/${getRandomNumber()}.jpg)`;
-    greeting.textContent = 'Good Morning';
-  } else if (hour < 18) {
-    // Day
-    document.body.style.backgroundImage = `url(./assets/images/day/${getRandomNumber()}.jpg)`;
-    greeting.textContent = 'Good Day';
-  } else {
-    // Evening
-    document.body.style.backgroundImage = `url(./assets/images/evening/${getRandomNumber()}.jpg)`;
-    greeting.textContent = 'Good Evening';
+// Check Time of Day
+const checkTimeOfDay = (h) => {
+  switch (h) {
+    case h<6:
+      return 'night';
+    case h<12:
+      return 'morning';
+    case h<18:
+      return 'day';
+    default:
+      return 'evening';
   }
 }
 
+// NEXT BACKGROUND
+const showNextBg = () => {
+  let hour = (new Date()).getHours();
+  let time = checkTimeOfDay(hour);
+  document.body.style.backgroundImage = `url(./assets/images/${time}/${getRandomNumber()}.jpg)`;
+}
+
+// BACKGROUND AND GREETING
+const setBgGreet = () => {
+  let hour = (new Date()).getHours();
+  let time = checkTimeOfDay(hour)
+  let GreetingTime = time.slice(0,1).toUpperCase() + time.slice(1);
+
+  document.body.style.backgroundImage = `url(./assets/images/${time}/${getRandomNumber()}.jpg)`;
+  greeting.textContent = `Good ${GreetingTime}`;
+
+  if (hour === 0) showDate();
+}
+
+
+
+
 // GET NAME and FOCUS
 const getName = () => {
-  (localStorage.getItem('name') === null)
+  (localStorage.getItem('name') === null || localStorage.getItem('name') == '')
     ? name.textContent = '[Enter Name]'
     : name.textContent = localStorage.getItem('name')
 }
 const getFocus = () => {
-  (localStorage.getItem('focus') === null)
+  (localStorage.getItem('focus') === null || localStorage.getItem('focus') == '')
     ? focus.textContent = '[Enter Focus]'
     : focus.textContent = localStorage.getItem('focus')
 }
@@ -82,6 +94,7 @@ const setName = (e) => {
     localStorage.setItem('name', e.target.innerText)
   }
 }
+
 const setFocus = (e) => {
   if (e.type === 'keypress') {
     if (e.which == 13 || e.keyCode == 13) {
@@ -93,10 +106,14 @@ const setFocus = (e) => {
   }
 }
 
+
+
+
 name.addEventListener('keypress', setName);
 name.addEventListener('blur', setName);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
+// nextBg.addEventListener('click', showNextBg);
 
 
 setInterval(showTime, 1000);
@@ -105,6 +122,6 @@ setBgGreet();
 getName();
 getFocus();
 // TODO
-// Change IMG every Hour 0min 0 sec
+// setInterval(showNextBg, 2000);
 // add Weather
 // 
