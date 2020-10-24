31ac57f2c9a4110ff03a9ad211c22a0d86636bd4
ShotAlex <shot.aleks@gmail.com>
2020-10-24 22:45:17 +0300

    reafactor: some styes

--- js/settings.js
+++ js/settings.js
@@ -2,19 +2,18 @@
 const checkboxLang = document.getElementById('checkbox-lang');
       // checkboxTemp = document.getElementById('checkbox-temp');
 
-
 // INIT APP
 const checkLocalStorage = () => {
   const lang = localStorage.getItem('lang') !== null ? localStorage.getItem('lang') : 'ru';
   const temp = localStorage.getItem('temp-metric') !== null ? localStorage.getItem('temp-metric') : 'c';
   const theme = localStorage.getItem('theme') !== null ? localStorage.getItem('theme') : 'dark';
-  console.log('checkLocalStorage ----');
-  console.log('lang :', lang);
-  console.log('temp :', temp);
-  console.log('theme :', theme);
+  const name = localStorage.getItem('name') !== null ? localStorage.getItem('name') : null;
+  const focus = localStorage.getItem('focus') !== null ? localStorage.getItem('focus') : null;
   localStorage.setItem('lang', lang)
   localStorage.setItem('temp-metric', temp)
   localStorage.setItem('theme', theme)
+  localStorage.setItem('name', name)
+  localStorage.setItem('focus', focus)
 }
 
 // CHANGE LANGUAGE
@@ -29,11 +28,7 @@ const changeLang = () => {
   // showQuote();
 }
 
-if (localStorage.getItem('lang') === 'ru') {
-  checkboxLang.checked = true;
-}
-
-
+if (localStorage.getItem('lang') === 'ru') checkboxLang.checked = true;
 
 checkboxLang.addEventListener('click', changeLang)
 checkLocalStorage();
